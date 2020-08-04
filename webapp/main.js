ee3f3d1d92e3bb4ddce38633640e83bc2af6ca8b
Nolonar <leowyrsch@outlook.com>
2020-08-04 19:52:51 +0200

    add alarm when timer ends

--- webapp/main.js
+++ webapp/main.js
@@ -1,5 +1,8 @@
-let inputString = prompt("Enter a duration for the timer. Valid units are: 'ms', 's', 'm', 'h'.", "20m");
+let validUnits = Object.keys(TIME_FROM_UNITS);
+let inputString = prompt(`Enter a duration for the timer. Valid units are:\n\n${validUnits.join("\n")}`, "20m");
 let result = inputString.match(/^(\d*\.?\d*)(ms|s|m|h)$/i);
+
+let alarm = document.getElementById("alarm");
 
 if (result) {
     let value = Number(result[1]);
@@ -8,7 +11,7 @@ if (result) {
 
     let timer = new Timer(document.getElementById("timer-canvas"));
     timer.onTimeUp.push(() => {
-        alert("Time's up!");
+        alarm.play();
     });
 
     timer.start(duration);
